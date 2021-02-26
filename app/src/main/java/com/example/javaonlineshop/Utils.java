d017462e3df424d5afd78331e72174ec176a4496
DESKTOP-0KMAREM\jovan <test>
2021-02-27 00:02:12 +0100

    #Lektion 03 - Items sortieren im Main Fragment

--- app/src/main/java/com/example/javaonlineshop/Utils.java
+++ app/src/main/java/com/example/javaonlineshop/Utils.java
@@ -26,6 +26,13 @@ public class Utils {
         }
     }
 
+    public static void clearSharedPreferences(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(DB_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+
     private static void initAllItems(Context context){
         ArrayList<GroceryItem> allItems = new ArrayList<>();
         GroceryItem milk = new GroceryItem("Milch",
@@ -33,6 +40,22 @@ public class Utils {
                 "https://cdn02.plentymarkets.com/f3fwwhiwe7th/item/images/71811280/full/Baerenmarke-Die-haltbare-Milch-3-8--Fett--Vollmilch-mit-Drehverschluss-1l--Milch-71811280.jpg",
                 "Trinken", 2.3, 10);
         allItems.add(milk);
+
+        GroceryItem iceCream = new GroceryItem("Ben & Jerry",
+                "Vanille-Doppelrahmglace mit Cookieteig-Stücken (mit Schokoladenstückchen) (16%) und kakaohaltigen Fettglasurstücken (1%).",
+                "https://i5.walmartimages.com/asr/6089ec90-dfea-4280-9dd2-a4e2972bf52e.9736d7870d96bd279b602f4f22a4f323.jpeg",
+                "Essen", 5.9, 15);
+        iceCream.setPopularityPoint(10);
+        iceCream.setUserPoint(7);
+        allItems.add(iceCream);
+
+        GroceryItem soda = new GroceryItem("Gerolsteiner",
+                "Sprudel, 0,5l",
+                "https://bestino.de/media/image/d7/1e/64/gerolsteiner-medium-0-75l-2439-001_600x600.jpg",
+                "Trinken", 1.18, 25);
+        soda.setPopularityPoint(6);
+        soda.setUserPoint(10);
+        allItems.add(soda);
 
         SharedPreferences sharedPreferences = context.getSharedPreferences(DB_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
